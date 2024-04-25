6d9b63db608e322d2136d1efaef732660d568930
정찬영 <q135794268020@gmail.com>
2024-04-25 12:26:51 +0900

    feat: Edit 페이지 디자인 구현 -#11

--- todo-list/src/pages/Edit.tsx
+++ todo-list/src/pages/Edit.tsx
@@ -1,5 +1,37 @@
+import styled from 'styled-components';
+import TodoList from '../components/TodoList';
+import Editor from '../components/Editor';
+import Header from '../common/Header';
+import Button from '../common/Button';
+
+const Wrapper = styled.div`
+  background-color: #ffffff;
+  width: 60vw;
+  height: 100vh;
+`;
+
+const TodoListWrapper = styled.div`
+  display: flex;
+  height: 92vh;
+`;
 const Edit = () => {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <Header
+        buttonVariety={
+          <Button buttonType={'default'} onClick={() => {}}>
+            {'Back'}
+          </Button>
+        }
+      >
+        {'Edit Todo'}
+      </Header>
+      <TodoListWrapper>
+        <TodoList />
+        <Editor />
+      </TodoListWrapper>
+    </Wrapper>
+  );
 };
 
 export default Edit;
